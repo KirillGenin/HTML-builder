3f45c8608810369945aa36cfdf20f1c999634ab7
KirillGenin <332211op@gmail.com>
2024-01-22 14:31:45 +0300

    feat: implement writing console input to file

---
+++ 02-write-file/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+const pathTofile = path.join(__dirname, 'text.txt');
+const writeStream = new fs.createWriteStream(pathTofile);
+
+process.stdout.write(
+  'Please enter text.\nNote: To exit, use CTRL+C or type exit.\n> ',
+);
+
+process.stdin.on('data', (data) => {
+  if (data.toString().trim() === 'exit') {
+    process.exit();
+  } else {
+    writeStream.write(data);
+    process.stdout.write('> ');
+  }
+});
+
+process.on('SIGINT', () => process.exit());
+
+process.on('exit', () => process.stdout.write('\nFile writing completed.\n'));
